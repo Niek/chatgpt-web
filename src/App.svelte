f98cbc931f8a86eb0888839e22ee37472129b21a
Niek van der Maas <mail@niekvandermaas.nl>
2023-03-02 22:41:41 +0400

    Add footer

--- src/App.svelte
+++ src/App.svelte
@@ -100,3 +100,16 @@
     </div>
   </div>
 </section>
+
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>ChatGPT-web</strong>
+      by
+      <a href="https://niekvandermaas.nl/">Niek van der Maas</a>
+      &mdash; see
+      <a href="https://github.com/Niek/chatgpt-web">GitHub</a>
+      for source code.
+    </p>
+  </div>
+</footer>
