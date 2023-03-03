ae01da35568415ee7550937fa032c47053e19478
Niek van der Maas <mail@niekvandermaas.nl>
2023-03-03 15:25:47 +0100

    Click on logo = reload

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -4,7 +4,7 @@
 
 <nav class="navbar" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="/">
+    <a class="navbar-item" href={"#"} on:click|preventDefault={() => (window.location = window.location)}>
       <img src={logo} alt="ChatGPT-web" width="28" height="28" />
       <p class="ml-2 is-size-4 has-text-weight-bold">ChatGPT-web</p>
     </a>
