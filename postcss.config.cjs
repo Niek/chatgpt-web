82aae80e675f3de3f482f8d53254e3b1d54e6ccd
Niek van der Maas <mail@niekvandermaas.nl>
2023-03-03 16:46:05 +0100

    Inline PostCSS config

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
-module.exports = {
-    plugins: [
-        purgecss({
-            content: ['./**/*.html', './**/*.svelte'],
-        })
-    ]
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+import purgecss from "@fullhuman/postcss-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        purgecss({
+          content: ["./**/*.html", "./**/*.svelte"],
+        }),
+      ],
+    },
+  },
+});
