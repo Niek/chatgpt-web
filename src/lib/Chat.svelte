bd719a94808f95f3752de654142ca6d89ba49a1b
ryanhex53 <ouyang.em@gmail.com>
2023-03-21 07:50:09 +0800

    fix textarea sizing problem; move down send btn while textbox high;

--- src/lib/Chat.svelte
+++ src/lib/Chat.svelte
@@ -426,7 +426,7 @@
   <progress class="progress is-small is-dark" max="100" />
 {/if}
 
-<form class="field has-addons has-addons-right" on:submit|preventDefault={() => submitForm()}>
+<form class="field has-addons has-addons-right is-align-items-flex-end" on:submit|preventDefault={() => submitForm()}>
   <p class="control is-expanded">
     <textarea
       class="input is-info is-focused chat-input"
