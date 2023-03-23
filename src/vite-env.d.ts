2f57e6069fc0e92fe7b918419702dbca99ddbdd0
Niek van der Maas <mail@niekvandermaas.nl>
2023-03-23 12:59:25 +0100

    Support prompts CSV

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,7 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+declare module '*/prompts.csv' {
+  const value: [{ act: string, prompt: string }]
+  export default value
+}
