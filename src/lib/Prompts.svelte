53ce9abb61450f24d7bcc843dae3d913fde77e20
Niek van der Maas <mail@niekvandermaas.nl>
2023-03-23 14:15:50 +0100

    Dropdown on click, not on hover

--- src/lib/Prompts.svelte
+++ src/lib/Prompts.svelte
@@ -5,16 +5,20 @@
     input.value = prompt
     input.style.height = 'auto'
     input.style.height = input.scrollHeight + 'px'
+    active = false
   }
 
   export let input : HTMLTextAreaElement
+
+  let active: boolean = false
 </script>
 
 {#if input}
 <div class="columns is-centered">
   <div class="column is-half">
-    <div class="dropdown is-hoverable is-fullwidth">
-      <div class="dropdown-trigger">
+    <div class="dropdown is-fullwidth" class:is-active={active}>
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div class="dropdown-trigger" on:click={() => { active = !active }}>
         <button class="button is-fullwidth" aria-haspopup="true" aria-controls="dropdown-menu">
           <span>Select a pre-made prompt</span>
           <span class="icon is-small">👇</span>
