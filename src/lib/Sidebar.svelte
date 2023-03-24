f6f17bfd0dbb2b1ae5f997e21ddcc26de914d6cd
Emil Elgaard <shivan@shivand.dk>
2023-03-24 09:57:42 -0400

    add confirmation prompt before deleting all chats

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -41,14 +41,13 @@
       >
     </li>
     <li>
-      <a
-        href={'#/'}
-        class="panel-block"
+      <a class="panel-block"
         class:is-disabled={!$apiKeyStorage}
         on:click={() => {
-          replace('#/').then(() => {
-            clearChats()
-          })
+          const confirmDelete = window.confirm('Are you sure you want to delete all your chats?')
+          if (confirmDelete) {
+            replace('#/').then(() => clearChats())
+          }
         }}><span class="greyscale mr-2">🗑️</span> Clear chats</a
       >
     </li>
