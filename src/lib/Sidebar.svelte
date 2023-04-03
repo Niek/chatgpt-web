02aa572d0b1e5c196f987841a9a0696deb6f3f2e
Emil Elgaard <shivan@shivand.dk>
2023-04-02 22:01:49 -0400

    convert sidebar to tailwind

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,94 +1,83 @@
 <script lang="ts">
-  import { params, replace } from 'svelte-spa-router'
+    import { params, replace } from 'svelte-spa-router'
 
-  import { apiKeyStorage, chatsStorage, clearChats, deleteChat } from './Storage.svelte'
-  import { exportAsMarkdown } from './Export.svelte'
+    import { apiKeyStorage, chatsStorage, clearChats, deleteChat } from './Storage.svelte'
+    import { exportAsMarkdown } from './Export.svelte'
+    import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte'
 
-  $: sortedChats = $chatsStorage.sort((a, b) => b.id - a.id)
+    $: sortedChats = $chatsStorage.sort((a, b) => b.id - a.id)
 
-  $: activeChatId = $params && $params.chatId ? parseInt($params.chatId) : undefined
+    $: activeChatId = $params && $params.chatId ? parseInt($params.chatId) : undefined
 
-  function delChat (chatId) {
-    if (activeChatId === chatId) {
-    // Find the max chatId other than the current one
-      const newChatId = sortedChats.reduce((maxId, chat) => {
-        if (chat.id === chatId) return maxId
-        return Math.max(maxId, chat.id)
-      }, 0)
+    function delChat (chatId) {
+      if (activeChatId === chatId) {
+        // Find the max chatId other than the current one
+        const newChatId = sortedChats.reduce((maxId, chat) => {
+          if (chat.id === chatId) return maxId
+          return Math.max(maxId, chat.id)
+        }, 0)
 
-      if (!newChatId) {
-        // No other chats, clear all and go to home
-        replace('/').then(() => { deleteChat(chatId) })
+        if (!newChatId) {
+          // No other chats, clear all and go to home
+          replace('/').then(() => {
+            deleteChat(chatId)
+          })
+        } else {
+          // Delete the current chat and go to the max chatId
+          replace(`/chat/${newChatId}`).then(() => {
+            deleteChat(chatId)
+          })
+        }
       } else {
-        // Delete the current chat and go to the max chatId
-        replace(`/chat/${newChatId}`).then(() => { deleteChat(chatId) })
+        deleteChat(chatId)
       }
-    } else {
-      deleteChat(chatId)
     }
-  }
 </script>
 
-<aside class="menu grow pt-4 ">
-  <p class="menu-label ml-3">Chats</p>
-  <ul class="menu-list">
-    {#if sortedChats.length === 0}
-      <li><a href={'#'} class="is-disabled">No chats yet...</a></li>
-    {:else}
-      <li>
-        <ul>
-          {#each sortedChats as chat}
-            <li class="group">
-              <a style="position: relative" href={`#/chat/${chat.id}`} class:is-disabled={!$apiKeyStorage} class:is-active={activeChatId === chat.id}>
-                <a class="is-pulled-right !hidden group-hover:!block px-1 py-0 grayscale has-text-weight-bold delete-button" href={'$'} on:click|preventDefault={() => delChat(chat.id)}>🗑️</a>
-                {chat.name || `Chat ${chat.id}`}
-              </a>
-            </li>
-          {/each}
-        </ul>
-      </li>
-    {/if}
-  </ul>
-  <div class="ml-3">
-   <p class="menu-label">Actions</p>
-  <ul class="menu-list">
-    <li>
-      <a href={'#/'} class="panel-block" class:is-disabled={!$apiKeyStorage} class:is-active={!activeChatId}
-        ><span class="grayscale mr-2">🔑</span> API key</a
-      >
-    </li>
-    <li>
-      <a href={'#/chat/new'} class="panel-block" class:is-disabled={!$apiKeyStorage}
-        ><span class="grayscale mr-2">➕</span> New chat</a
-      >
-    </li>
-    <li class="group">
-      <a class="panel-block"
-        href="{'#/'}"
-        class:is-disabled={!$apiKeyStorage}
-        on:click|preventDefault={() => {
+<Sidebar>
+    <SidebarWrapper>
+        <SidebarGroup ulClass="my-0">
+            {#each sortedChats as chat}
+
+                <li class="group flex relative flex-row justify-between items-center dark:hover:bg-gray-700 rounded-lg" class:bg-gray-700={activeChatId === chat.id}>
+                    <a class="flex items-center grow justify-between dark:text-white p-2 text-base font-normal text-gray-900"
+                       href={`#/chat/${chat.id}`}>{chat.name || `Chat ${chat.id}`}</a>
+                    <a class="absolute ml-auto right-0 hidden group-hover:!inline px-1 py-0 grayscale group/del" href={'$'} on:click|preventDefault={() => delChat(chat.id)}>🗑️</a>
+                </li>
+
+            {/each}
+        </SidebarGroup>
+
+        <SidebarGroup border>
+            <SidebarItem label="Api key" active={!activeChatId}>
+                <a href="#/" slot="icon" class="grayscale">🔑</a>
+            </SidebarItem>
+            <SidebarItem label="New chat" href={'#/chat/new'}>
+                <span slot="icon" class="grayscale">➕</span>
+            </SidebarItem>
+
+            <SidebarItem label="Clear chats" href="{'#/'}" on:click={(e) => {
+            e.preventDefault()
+            if (!apiKeyStorage) return
           const confirmDelete = window.confirm('Are you sure you want to delete all your chats?')
           if (confirmDelete) {
             replace('#/').then(() => clearChats())
           }
-        }}><span class="grayscale mr-2">🗑️</span> Clear chats</a
-      >
-    </li>
-    {#if activeChatId}
-      <li>
-        <a
-          href={'#/'}
-          class="panel-block"
-          class:is-disabled={!apiKeyStorage}
-          on:click|preventDefault={() => {
-            if (activeChatId) {
+        }}>
+                <span slot="icon" class="grayscale">🗑️</span>
+            </SidebarItem>
+
+            <SidebarItem label="Export chat" on:click={(e) => {
+         e.preventDefault()
+         if (!apiKeyStorage) return
+         if (activeChatId) {
               exportAsMarkdown(activeChatId)
             }
-          }}><span class="grayscale mr-2">📥</span> Export chat</a
-        >
-      </li>
-    {/if}
-  </ul>
-    </div>
+          }}>
+                <span slot="icon" class="grayscale">📥</span>
+            </SidebarItem>
 
-</aside>
+        </SidebarGroup>
+
+    </SidebarWrapper>
+</Sidebar>
