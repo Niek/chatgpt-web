070b519c693334a06f14c91964d98c67528a9b66
Emil Elgaard <shivan@shivand.dk>
2023-04-03 13:52:15 -0400

    fix max width of page.

--- src/App.svelte
+++ src/App.svelte
@@ -43,14 +43,14 @@
 <Navbar />
 
 <section class="flex flex-col grow">
-    <div class="flex flex-col grow gap-x-2 items-stretch md:flex-row">
-      <div id="sidebar" class="flex flex-col justify-end basis-0 md:basis-1/5 p-4">
+    <div class="flex flex-col grow gap-x-2 items-stretch md:flex-row mx-auto w-full max-w-[1936px]">
+      <div id="sidebar" class="flex flex-col justify-end basis-1/5 p-4">
         <Sidebar />
         <!--Spacer element-->
         <Alert color="opacity-0" class="px-3 py-2"><div class="invisible p-4">_</div></Alert>
 
       </div>
-      <div  id="content" class="flex grow basis-0 md:basis-4/5 flex-col p-4">
+      <div  id="content" class="flex  flex-col grow p-4">
         {#key $location}
           <Router {routes} on:conditionsFailed={() => replace('/')}/>
         {/key}
