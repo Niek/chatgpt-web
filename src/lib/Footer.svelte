4d51599998db3fadb6871082d44410462e505d05
Emil Elgaard <shivan@shivand.dk>
2023-04-02 20:15:48 -0400

    use footer html tag for footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,5 @@
-<div class="text-center flex items-center justify-center py-12 bg-gray-900">
+<footer class="text-center flex items-center justify-center py-12 bg-gray-900">
   <div class="text-white mx-auto text-center">
     <strong>ChatGPT-web</strong> by <a class="text-blue-400 hover:underline" href="https://niekvandermaas.nl/">Niek van der Maas</a> —  see <a class="text-blue-400 hover:underline" href="https://github.com/Niek/chatgpt-web">GitHub</a> for source code.
   </div>
-</div>
+</footer>
