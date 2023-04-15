2d0050fae5a577473f2c853c9eccf89db65d1be3
Emil Elgaard <shivan@shivan.dk>
2023-04-15 01:06:28 -0400

    fix problem with build by removing purgecss
    (cherry picked from commit d380706ff671e1d6e7055b37d9f29047031520e0)

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,13 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import dsv from '@rollup/plugin-dsv'
 
-import purgecss from '@fullhuman/postcss-purgecss'
 
 const plugins = [svelte(), dsv()]
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
   // Only run PurgeCSS in production builds
-  if (command === 'build') {
-    return {
-      plugins,
-      css: {
-        postcss: {
-          plugins: [
-            purgecss({
-              content: ['./**/*.html', './**/*.svelte'],
-              safelist: ['pre', 'code']
-            })
-          ]
-        }
-      }
-    }
-  } else {
-    return {
-      plugins
-    }
+  return {
+    plugins
   }
 })
