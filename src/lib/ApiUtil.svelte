867c6d7e3302bf81441e3a4c4d07f03e06024310
Webifi <john@webifi.com>
2023-06-07 11:52:08 -0500

    Fix endpoint

--- src/lib/ApiUtil.svelte
+++ src/lib/ApiUtil.svelte
@@ -2,7 +2,7 @@
   // This makes it possible to override the OpenAI API base URL in the .env file
   const apiBase = import.meta.env.VITE_API_BASE || 'https://api.openai.com'
   const endpointCompletions = import.meta.env.VITE_ENDPOINT_COMPLETIONS || '/v1/chat/completions'
-  const endpointModels = import.meta.env.VITE_ENDPOINT_COMPLETIONS || '/v1/models'
+  const endpointModels = import.meta.env.VITE_ENDPOINT_MODELS || '/v1/models'
 
   export const getApiBase = ():string => apiBase
   export const getEndpointCompletions = ():string => endpointCompletions
