4353a8c18469072eb9f0cc0fa2a88c98b8060d63
Webifi <john@webifi.com>
2023-06-11 19:05:23 -0500

    Add overrides on summary continue

--- src/lib/ChatRequest.svelte
+++ src/lib/ChatRequest.svelte
@@ -159,7 +159,7 @@ export class ChatRequest {
                   // ?? anything to do when "[DONE]"?
                   } else {
                     const data = JSON.parse(ev.data)
-                    // console.log('data', data)
+                    console.log('data', data)
                     window.requestAnimationFrame(() => { chatResponse.updateFromAsyncResponse(data) })
                   }
                 }
@@ -378,7 +378,8 @@ export class ChatRequest {
               return await _this.sendRequest(chat.messages, {
                 ...opts,
                 didSummary: true
-              })
+              },
+              overrides)
             }
           } catch (e) {
             _this.updating = false
