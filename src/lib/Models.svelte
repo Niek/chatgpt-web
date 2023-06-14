b2721c965d27adc77783a65e1fcc761ae445e241
Webifi <john@webifi.com>
2023-06-13 19:14:37 -0500

    Add gpt-3.5-turbo-0613 and gpt-3.5-turbo-16k

--- src/lib/Models.svelte
+++ src/lib/Models.svelte
@@ -14,10 +14,20 @@ const modelDetails : Record<string, ModelDetail> = {
         completion: 0.00006, // $0.06 per 1000 tokens completion
         max: 8192 // 8k max token buffer
       },
+      'gpt-3.5-turbo-0613': {
+        prompt: 0.0000015, // $0.0015 per 1000 tokens prompt
+        completion: 0.0000015, // $0.0015 per 1000 tokens completion
+        max: 4096 // 4k max token buffer
+      },
       'gpt-3.5': {
         prompt: 0.000002, // $0.002 per 1000 tokens prompt
         completion: 0.000002, // $0.002 per 1000 tokens completion
         max: 4096 // 4k max token buffer
+      },
+      'gpt-3.5-turbo-16k': {
+        prompt: 0.000003, // $0.003 per 1000 tokens prompt
+        completion: 0.000004, // $0.004 per 1000 tokens completion
+        max: 16384 // 16k max token buffer
       }
 }
 
@@ -35,7 +45,9 @@ export const supportedModels : Record<string, ModelDetail> = {
       'gpt-4-32k': modelDetails['gpt-4-32k'],
       'gpt-4-32k-0314': modelDetails['gpt-4-32k'],
       'gpt-3.5-turbo': modelDetails['gpt-3.5'],
-      'gpt-3.5-turbo-0301': modelDetails['gpt-3.5']
+      'gpt-3.5-turbo-16k': modelDetails['gpt-3.5-turbo-16k'],
+      'gpt-3.5-turbo-0301': modelDetails['gpt-3.5'],
+      'gpt-3.5-turbo-0613': modelDetails['gpt-3.5-turbo-0613']
 }
 
 const lookupList = {
