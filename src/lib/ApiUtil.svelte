e33e06399aefd717d6357da01feb292d5c38a168
CamB <12563194+cablackmon@users.noreply.github.com>
2023-07-03 14:59:10 -0500

    Update ApiUtil.svelte

--- src/lib/ApiUtil.svelte
+++ src/lib/ApiUtil.svelte
@@ -1,12 +1,9 @@
 <script context="module" lang="ts">
-  // This makes it possible to override the OpenAI API base URL in the .env file
-  const apiBase = import.meta.env.VITE_API_BASE || 'https://api.openai.com'
-  const endpointCompletions = import.meta.env.VITE_ENDPOINT_COMPLETIONS || '/v1/chat/completions'
-  const endpointGenerations = import.meta.env.VITE_ENDPOINT_GENERATIONS || '/v1/images/generations'
-  const endpointModels = import.meta.env.VITE_ENDPOINT_MODELS || '/v1/models'
-
+  const apiBase = process.env.VITE_API_BASE || 'https://api.openai.com'
+  const endpointCompletions = process.env.VITE_ENDPOINT_COMPLETIONS || '/v1/chat/completions'
+  const endpointModels = process.env.VITE_ENDPOINT_MODELS || '/v1/models'
+  
   export const getApiBase = ():string => apiBase
   export const getEndpointCompletions = ():string => endpointCompletions
-  export const getEndpointGenerations = ():string => endpointGenerations
   export const getEndpointModels = ():string => endpointModels
 </script>
