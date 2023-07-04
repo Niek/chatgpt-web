3c3c2bd5a59e1bde34422900c48f863801175e1f
Edward Sariev <sarieved@gmail.com>
2023-07-04 15:11:41 +0300

    Fix supported models

--- src/lib/Types.svelte
+++ src/lib/Types.svelte
@@ -1,11 +1,6 @@
 <script context="module" lang="ts">
   export const supportedModels = [ // See: https://platform.openai.com/docs/models/model-endpoint-compatibility
-    'gpt-4',
-    'gpt-4-0314',
-    'gpt-4-32k',
-    'gpt-4-32k-0314',
-    'gpt-3.5-turbo',
-    'gpt-3.5-turbo-0301'
+    'gpt-3.5-turbo'
   ]
   export type Model = typeof supportedModels[number];
 
