deb75832e765c6c214db6452f22dcc43d1282995
Webifi <john@webifi.com>
2023-08-19 13:09:43 -0500

    specify profile for new chat via uri

--- src/lib/NewChat.svelte
+++ src/lib/NewChat.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+  import { querystring } from 'svelte-spa-router'
   import { addChat } from './Storage.svelte'
   import { replace } from 'svelte-spa-router'
+  import { getProfile } from './Profiles.svelte'
 
   // Create the new chat instance then redirect to it
-  const chatId = addChat()
+
+  const urlParams: URLSearchParams = new URLSearchParams($querystring)
+  const chatId = urlParams.has('p') ? addChat(getProfile(urlParams.get('p') || '')) : addChat()
+
   replace(`/chat/${chatId}`)
 </script>
