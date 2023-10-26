bc37e38222e16609120455c63713d0d931f1e393
Mitchell Byrden <mitch@xpulsenetwork.com>
2023-10-27 05:39:25 +1100

    Fix Javascript on index

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import dsv from '@rollup/plugin-dsv';
+import purgecss from '@fullhuman/postcss-purgecss';
+const plugins = [svelte(), dsv()];
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    // Only run PurgeCSS in production builds
+    if (command === 'build') {
+        return {
+            plugins,
+            css: {
+                postcss: {
+                    plugins: [
+                        purgecss({
+                            content: ['./**/*.html', './**/*.svelte'],
+                            safelist: ['pre', 'code']
+                        })
+                    ]
+                }
+            },
+            base: './'
+        };
+    }
+    else {
+        return {
+            plugins
+        };
+    }
+});
+//# sourceMappingURL=vite.config.js.map
