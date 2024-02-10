7ec79046cb8b8ef276578af518492e726b1c4344
Chih-Wei Chang <2840571+lazywei@users.noreply.github.com>
2024-02-10 09:07:28 -0800

    Allow using URL Params to jumpstart NewChat with a given message.
    For example,
    ```
    https://localhost:5173/#/chat/new?message=hello
    ```
    
    This will start a new chat and then send the message out to kickoff the session.
    
    Issue: https://github.com/Niek/chatgpt-web/issues/418

--- src/lib/NewChat.svelte
+++ src/lib/NewChat.svelte
@@ -1,20 +1,41 @@
 <script lang="ts">
   import { querystring } from 'svelte-spa-router'
-  import { addChat, setChatSettingValueByKey } from './Storage.svelte'
+  import {
+    addChat,
+    addMessage,
+    setChatSettingValueByKey,
+    submitExitingPromptsNow,
+  } from './Storage.svelte'
   import { replace } from 'svelte-spa-router'
   import { getProfile, restartProfile } from './Profiles.svelte'
   import { getChatDefaults, hasChatSetting } from './Settings.svelte'
+  import { v4 as uuidv4 } from 'uuid'
+  import { type Message } from './Types.svelte'
 
   // Create the new chat instance then redirect to it
 
   const urlParams: URLSearchParams = new URLSearchParams($querystring)
-  const chatId = urlParams.has('p') ? addChat(getProfile(urlParams.get('p') || '')) : addChat()
-  Object.keys(getChatDefaults()).forEach(k => {
+  const chatId = urlParams.has("p")
+    ? addChat(getProfile(urlParams.get("p") || ""))
+    : addChat()
+  Object.keys(getChatDefaults()).forEach((k) => {
     if (urlParams.has(k) && hasChatSetting(k as any)) {
       setChatSettingValueByKey(chatId, k as any, urlParams.get(k))
     }
   })
-  
+
   restartProfile(chatId)
+  const messageContent = urlParams.get("message")
+  if (messageContent !== null) {
+    const uuid = uuidv4()
+    const inputMessage: Message = {
+      role: "user",
+      content: messageContent,
+      uuid,
+    }
+    addMessage(chatId, inputMessage)
+    $submitExitingPromptsNow = true
+  }
+
   replace(`/chat/${chatId}`)
 </script>
